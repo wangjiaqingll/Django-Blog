dfd6b7378ab8e17cc2d2b1d144caad5261dcd1eb
wangjiaqingll <wangjiaqing.ll@qq.com>
2020-08-22 10:28:50 +0800

    add host

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '9bt926nid=x*$%xr$j@=(ia4*-fb!+pi@8-wkq3c+aa-7nkt2r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.bom-assistant.top']
 
 
 # Application definition
